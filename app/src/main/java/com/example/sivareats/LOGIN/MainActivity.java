a653e4a4e932f031f0b9538fb0f0c24cf6ad629f
Giova2001 <gv21033@ues.edu.sv>
2025-10-23 12:13:06 -0600

    splash activity,login activity, editprofile activity, icono de retornar y logo.png

--- app/src/main/java/com/example/sivareats/LOGIN/MainActivity.java
+++ app/src/main/java/com/example/sivareats/LOGIN/MainActivity.java
@@ -2,33 +2,25 @@ package com.example.sivareats.LOGIN;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.example.sivareats.R;
-import com.example.sivareats.ui.NavegacionActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SPLASH_DELAY = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        // Después de verificar credenciales
-        Intent intent = new Intent(MainActivity.this, NavegacionActivity.class);
-        intent.putExtra("USER_TYPE", "user"); // "user", "delivery", "restaurant", "admin"
-        startActivity(intent);
-        finish();
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_DELAY);
     }
 }
